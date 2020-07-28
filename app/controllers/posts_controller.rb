3841afb78721ad4a0da89d6bc3903ebb3149b617
kinopyyy <sss.namiki@gmail.com>
2020-07-28 13:18:13 +0900

    add show,edit,destroy,update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,37 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @post = Post.new
   end
 
   def new
     @post = Post.new
   end
 
-
   def create    
     Post.create(post_params)   
-  end  
+    redirect_to root_path  
+  end 
+  
+  def show
+    @post = Post.find(params[:id])
+  end
 
- 
+  def edit
+    @post = Post.find(params[:id])
+  end
 
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to root_path     
+  end
 
   private
   def post_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "posts#index"  
   # resources :posts do 
-  resources :posts, only: [:new, :create, :index]
+  resources :posts, only: [:new, :create, :index, :show,:edit,:update, :destroy]
   
 
 end
