ecda0a97cfcde708d10ce23b464022f1db15ff1a
kinopyyy <sss.namiki@gmail.com>
2020-08-12 20:39:56 +0900

    update edit front

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,11 +26,11 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-  def update
+  def update  
     post = Post.find(params[:id])
     post.update(post_params)
-    # redirect_to user_path(current_user)  
-    redirect_to post_path(post.id)
+    redirect_to user_path(current_user)  
+    # redirect_to post_path(post.id)
   end
 
   def destroy
